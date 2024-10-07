896148aae62fab32ef19ebda36de58adf42d5293
akhlakm <me@akhlakm.com>
2024-10-06 22:42:23 -0400

    Add script execution functionality

--- auto.go
+++ auto.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"gomaker/internal/fileio"
 	"gomaker/internal/logger"
+	"gomaker/internal/runner"
+	"os"
+	"path/filepath"
 )
 
-func main() {
-	logger.Trace("main", "")
-	logger.Print("Hello, World!")
-	logger.Print("~")
+func Run(script string, args []string) {
+	logger.Trace("Auto-Run", script)
+
+	// Get the path of the executable.
+	ex, err := os.Executable()
+	if err != nil {
+		logger.Error("Auto-Run", "", "Failed to get executable path")
+		return
+	}
+
+	// Join the path of the script.
+	scriptPath := filepath.Join(filepath.Dir(ex), script)
+
+	// Check if the script exists.
+	if !fileio.FileExists(scriptPath) {
+		logger.Error("Auto-Run", scriptPath, "Failed to find script")
+		return
+	}
+
+	// Run the script with the arguments.
+	ok, err := runner.Execute(scriptPath, args, "")
+	if !ok {
+		logger.Error("Auto-Run", scriptPath, fmt.Sprintf("Failed: %s", err))
+	}
 }
